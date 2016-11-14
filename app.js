bef2393257361e884315e6e28d1458892829f4dd
AnEpicName <matiashern1321@gmail.com>
2016-11-14 20:10:15 -0300

    Conectado con mongodb

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index'),
+    users = require('./routes/users');
 
 var app = express();
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('We are conectados.')
+});
+
+mongoose.connect('mongodb://localhost/EventFinder');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
